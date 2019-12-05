8760bdd6429bead1b7b45e5d0bcea6114659820b
lucedeswede <nathanielcarolina@gmail.com>
2019-12-05 22:25:01 +0100

    Update overall table layout and formatting

--- src/employeeInformation.js
+++ src/employeeInformation.js
@@ -20,14 +20,15 @@ class EmployeeInformation extends Component {
         .catch(err => console.error(err));
     }
 
-    renderEmployee = ({ Emp_ID, department, LName, FName, Position_Name }) =>
+    renderEmployee = ({ Emp_ID, department, LName, FName, Position_Name, Emp_Status }) =>
         <tr key={Emp_ID}>
             <td>{Emp_ID}</td>
             <td>{department}</td>
             <td>{LName}, {FName}</td>
             <td>{Position_Name}</td>
+            <td>{Emp_Status}</td>
             <td><a href="#" className="text-success">more info</a></td>
-        </tr>        
+        </tr>
 
     render() {
 
@@ -35,75 +36,83 @@ class EmployeeInformation extends Component {
 
         return (
             <>
-                <h2 className="display-6">Employee Information</h2>
+                <h4 className="">Employee Information</h4>
                 <div className="my-4 p-3 bg-gray rounded">
                     <p className="small text-uppercase text-muted m-0">Filter by:</p>
                     <form class="form-inline"> 
-                        <label class="mr-2" for="filterDepartment">Department:</label>
-                        <select class="mr-3" id="filterDepartment">
-                        <option selected>Select Department</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                        </select>
 
-                        <label class="mr-2" for="filterStatus">Status:</label>
-                        <select class="mr-3" id="filterStatus">
-                        <option selected>Select Status</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                        </select>
+                        <div className="form-group">
+                            <label class="mr-2" for="filterDepartment">Department:</label>
+                            <select class="mr-3 form-control" id="filterDepartment">
+                                <option selected>None selected</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
 
-                        <label class="mr-2" for="filterPosition">Position:</label>
-                        <input type="text" class="form-control mr-3" id="filterPosition" />
-                        
-                        <label class="mr-2" for="filterName">Name:</label>
-                        <input type="text" class="form-control mr-3" id="filterName" />
-                        
+                        <div className="form-group">
+                            <label class="mr-2" for="filterName">Name:</label>
+                            <input type="text" class="form-control mr-3" id="filterName" />
+                        </div> 
+
+                        <div className="form-group">
+                            <label class="mr-2" for="filterPosition">Position:</label>
+                            <input type="text" class="form-control mr-3" id="filterPosition" />
+                        </div>
+
+                        <div className="form-group">
+                            <label class="mr-2" for="filterStatus">Status:</label>
+                            <select class="mr-3 form-control" id="filterStatus">
+                            <option selected>None selected</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                            </select>
+                        </div>
+
                         <button type="submit" class="btn btn-secondary mr-2">Apply</button>
                         <button type="submit" class="btn btn-outline-secondary">Reset</button>
                     </form>
 
                 </div>
 
-                <table class="table table-hover sortable">
+                <div className="row mb-2">
+                    <div className="col-6 text-muted"><span>Showing 1-5 of 10</span></div>
+                    <div className="col-6 text-right">
+                        <select className="mr-4 text-muted" id="numberRecords">
+                            <option value="5">5 per page</option>
+                            <option value="10">10 per page</option>
+                            <option value="25">25 per page</option>
+                            <option value="50">50 per page</option>
+                        </select>
+                        <select className="mr-2 text-muted" id="currentPage">
+                            <option value="1">Page 1</option>
+                            <option value="2">Page 2</option>
+                        </select>
+                        <label className="mr-2 text-muted" for="currentPage">of 2</label>
+                        <div class="btn-group btn-group-sm border border-secondary rounded-lg" role="group" aria-label="Next and previous page">
+                            <button type="button" class="btn btn-light py-0"><i class="fas fa-chevron-left text-muted"></i></button>
+                            <button type="button" class="btn btn-light border-left py-0"><i class="fas fa-chevron-right text-muted"></i></button>
+                        </div>
+                    </div>
+                </div>
+
+                <table class="table table-hover">
                     <thead>
                         <tr>
-                            <th scope="col" data-defaultsign='nospan'>Employee ID</th>
-                            <th scope="col" data-defaultsign='nospan'>Department</th>
-                            <th scope="col" data-defaultsign='nospan'>Name</th>
-                            <th scope="col" data-defaultsign='nospan'>Position</th>
-                            <th scope="col" data-defaultsort='disabled'>Details</th>
+                            <th scope="col" className='nosort'>ID</th>
+                            <th scope="col" className='nosort'>Department</th>
+                            <th scope="col" className='nosort'>Name</th>
+                            <th scope="col" className='nosort'>Position</th>
+                            <th scope="col" className='nosort'>Status</th>
+                            <th scope="col">Details</th>
                         </tr>
                     </thead>
                     <tbody>
                         {employees.map(this.renderEmployee)}
                     </tbody>
                 </table>
-
-                <div className="row">
-                    <div className="col-6">
-                        <nav aria-label="Page navigation example">
-                        <ul class="pagination">
-                            <li class="page-item"><a class="page-link text-success" href="#">Previous</a></li>
-                            <li class="page-item"><a class="page-link text-success" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link text-success" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link text-success" href="#">3</a></li>
-                            <li class="page-item"><a class="page-link text-success" href="#">Next</a></li>
-                        </ul>
-                        </nav>
-                    </div>
-
-                    <div className="col-6 text-right">
-                        <label class="mr-2" for="numberRecords">Number of records per page:</label>
-                        <select class="mr-4" id="numberRecords">
-                        <option value="10">10</option>
-                        <option value="20">20</option>
-                        <option value="30">30</option>
-                        </select>
-                    </div>
-                </div>
             </>
         );
     }
