1ea03b3a4a5483695d61adf3753ea10da7003f52
lucedeswede <nathanielcarolina@gmail.com>
2019-12-08 21:36:35 +0100

    Change "for" to "htmlFor"

--- src/loginpage.js
+++ src/loginpage.js
@@ -9,9 +9,9 @@ class loginpage extends Component {
               
                         <img className="mb-4" src={logo} alt="" width="100" height="100"/>
                         <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-                        <label for="inputEmail" className="sr-only">Email address</label>
+                        <label htmlFor="inputEmail" className="sr-only">Email address</label>
                         <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required="" autofocus=""/>
-                        <label for="inputPassword" className="sr-only">Password</label>
+                        <label htmlFor="inputPassword" className="sr-only">Password</label>
                         <input type="password" id="inputPassword" className="form-control" placeholder="Password" required=""/>
                         <div className="checkbox mb-3">
                         <label>
