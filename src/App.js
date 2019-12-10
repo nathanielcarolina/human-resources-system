ed647aa032e716a191dc8f504837799d4f5eef23
lucedeswede <nathanielcarolina@gmail.com>
2019-12-10 15:03:58 +0100

    Update view to use Router

--- src/App.js
+++ src/App.js
@@ -23,98 +23,11 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="row">
-        <div className="col-2 pr-0 border-right"><Sidebar /></div>
-        
+        <div className="col-2 pr-0 border-right">
+          <Sidebar />
+        </div>
         <div className="col-10 p-4">
-          <div className="tab-content" id="nav-tabContent">
-            <Router />
-
-
-
-
-            <div 
-              className="tab-pane fade show active" 
-              id="list-employee-information" 
-              role="tabpanel" 
-              aria-labelledby="list-employee-information">
-              <EmployeeInformation />
-            </div>
-            <div 
-              className="tab-pane fade" 
-              id="list-department-admin" 
-              role="tabpanel" 
-              aria-labelledby="list-department-admin">
-              <DepartmentAdministrative />
-            </div>
-            <div 
-              className="tab-pane fade" 
-              id="list-department-development" 
-              role="tabpanel" 
-              aria-labelledby="list-department-development">
-              <DepartmentDevelopment />
-            </div>
-            <div 
-              className="tab-pane fade" 
-              id="list-department-finance" 
-              role="tabpanel" 
-              aria-labelledby="list-department-finance">
-              <DepartmentFinance />
-            </div>
-            <div 
-              className="tab-pane fade" 
-              id="list-department-hr" 
-              role="tabpanel" 
-              aria-labelledby="list-department-hr">
-              <DepartmentHumanResources />
-            </div>
-            <div 
-              className="tab-pane fade" 
-              id="list-department-itSupport" 
-              role="tabpanel" 
-              aria-labelledby="list-department-itSupport">
-              <DepartmentITSupport />
-            </div>
-            <div 
-              className="tab-pane fade" 
-              id="list-department-qualityAssurance" 
-              role="tabpanel" 
-              aria-labelledby="list-department-qualityAssurance">
-              <DepartmentQualityAssurance />
-            </div>
-            <div 
-              className="tab-pane fade" 
-              id="list-payroll" 
-              role="tabpanel" 
-              aria-labelledby="list-payroll">
-              <Payroll />
-            </div>
-            <div 
-              className="tab-pane fade" 
-              id="list-leave-management" 
-              role="tabpanel" 
-              aria-labelledby="list-leave-management">
-              <LeaveManagement />
-            </div>
-            <div className="tab-pane fade" id="list-performance" role="tabpanel" aria-labelledby="list-performance">
-              <Performance />
-            </div>
-            <div className="tab-pane fade" id="list-candidate-form" role="tabpanel" aria-labelledby="list-candidate-form">
-              <Candidate />
-            </div>
-            <div className="tab-pane fade" id="list-employee-form" role="tabpanel" aria-labelledby="list-employee-form">
-              <AddEmployee />
-            </div>
-            <div 
-              className="tab-pane fade" 
-              id="list-equipment" 
-              role="tabpanel" 
-              aria-labelledby="list-equipment">
-              <Equipment />
-            </div>
-            <div className="tab-pane fade" id="list-separation" role="tabpanel" aria-labelledby="list-separation">
-              <p>test</p>
-            </div>
-          </div>
+          <Router />
         </div>
       </div>
     </div>
