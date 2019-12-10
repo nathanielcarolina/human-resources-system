b48e0eebc6d08b11e77576a86955ac731de8b4a1
lucedeswede <nathanielcarolina@gmail.com>
2019-12-10 15:06:36 +0100

    Replace all a tags with Link or NavLink

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import employeePicture from './employeePicture.jpg';
+import { Link, NavLink } from "react-router-dom";
 
 class Sidebar extends Component {
 
@@ -33,151 +34,116 @@ class Sidebar extends Component {
                         </div>
                     </div>
                     <div id="sidebar" className="list-group list-group-flush" role="tablist">
-                        <a 
-                            href="#list-employee-information" 
-                            id="list-employee-information-list" 
-                            data-toggle="list" 
-                            role="tab" 
-                            aria-controls="employee-information" 
-                            className="list-group-item list-group-item-action list-group-item-light active">
-                                <i className="fas fa-users mr-2"></i>
-                                Employee Information
-                        </a>
-                        <a 
-                            href="#list-departments" 
+
+                        <NavLink to="/employee-information"
+                            className="list-group-item list-group-item-action list-group-item-light"
+                            role="tab" >
+                            <i className="fas fa-users mr-2"></i>
+                            Employee Information
+                        </NavLink>
+
+                        <Link to="#"
                             id="list-departments-list" 
                             data-toggle="list" 
                             role="tab" 
-                            aria-controls="departments" 
                             className="list-group-item list-group-item-action list-group-item-light"
                             onClick={this.toggleVisibilityDepartments} >
                                 <i className="fas fa-sitemap mr-2"></i>
                                 Departments
                                 <i className="fas fa-chevron-down float-right pt-1"></i>
-                        </a>
-                        <a 
-                            href="#list-department-admin" 
-                            id="list-department-admin-list" 
-                            data-toggle="list" 
-                            role="tab" 
-                            aria-controls="department-admin" 
-                            className="list-group-item list-group-item-action list-group-item-light departments-list d-none">
-                                <i className="fas fa-copy mr-2 ml-4"></i>
-                                Administrative
-                        </a>
-                        <a 
-                            href="#list-department-development" 
-                            id="list-department-development-list" 
-                            data-toggle="list" 
-                            role="tab" 
-                            aria-controls="department-development" 
-                            className="list-group-item list-group-item-action list-group-item-light departments-list d-none">
-                                <i className="fas fa-code mr-2 ml-4"></i>
-                                Development
-                        </a>
-                        <a 
-                            href="#list-department-finance" 
-                            id="list-department-finance-list" 
-                            data-toggle="list" 
-                            role="tab" 
-                            aria-controls="department-finance" 
-                            className="list-group-item list-group-item-action list-group-item-light departments-list d-none">
-                                <i className="fas fa-file-invoice-dollar mr-2 ml-4"></i>
-                                Finance
-                        </a>
-                        <a 
-                            href="#list-department-hr" 
-                            id="list-department-hr-list" 
-                            data-toggle="list" 
-                            role="tab" 
-                            aria-controls="department-hr" 
-                            className="list-group-item list-group-item-action list-group-item-light departments-list d-none">
-                                <i className="fas fa-portrait mr-2 ml-4"></i>
-                                Human Resources
-                        </a>
-                        <a 
-                            href="#list-department-itSupport" 
-                            id="list-department-itSupport-list" 
-                            data-toggle="list" 
-                            role="tab" 
-                            aria-controls="department-itSupport" 
-                            className="list-group-item list-group-item-action list-group-item-light departments-list d-none">
-                                <i className="fas fa-question-circle mr-2 ml-4"></i>
-                                IT Support
-                        </a>
-                        <a 
-                            href="#list-department-qualityAssurance" 
-                            id="list-department-qualityAssurance-list" 
-                            data-toggle="list" 
-                            role="tab" 
-                            aria-controls="department-qualityAssurance" 
-                            className="list-group-item list-group-item-action list-group-item-light departments-list d-none">
-                                <i className="fas fa-list-alt mr-2 ml-4"></i>
-                                Quality Assurance
-                        </a>
-                        <a 
-                            href="#list-payroll" 
-                            id="list-payroll-list" 
-                            data-toggle="list" 
-                            role="tab" 
-                            aria-controls="payroll" 
-                            className="list-group-item list-group-item-action list-group-item-light">
-                                <i className="fas fa-money-bill mr-2"></i>
-                                Payroll
-                        </a>
-                        <a 
-                            href="#list-leave-management" 
-                            id="list-leave-management-list" 
-                            data-toggle="list" 
-                            role="tab" 
-                            aria-controls="leave-management" 
-                            className="list-group-item list-group-item-action list-group-item-light">
-                                <i className="fas fa-bed mr-2"></i>
-                                Leave Management
-                        </a>
-                        <a 
-                            href="#list-performance" 
-                            id="list-performance-list" 
-                            data-toggle="list" 
-                            role="tab" 
-                            aria-controls="performance" 
-                            className="list-group-item list-group-item-action list-group-item-light">
-                                <i className="fas fa-chart-line mr-2"></i>
-                                Performance
-                        </a>
-                        <a 
-                            href="#list-recruitment" 
+                        </Link>
+
+                        <Link to="/departments/administrative"
+                            className="list-group-item list-group-item-action list-group-item-light departments-list d-none"
+                            role="tab" >
+                            <i className="fas fa-copy mr-2 ml-4"></i>
+                            Administrative
+                        </Link>
+
+                        <Link to="/departments/development"
+                            className="list-group-item list-group-item-action list-group-item-light departments-list d-none"
+                            role="tab" >
+                            <i className="fas fa-code mr-2 ml-4"></i>
+                            Development
+                        </Link>
+
+                        <Link to="/departments/finance"
+                            className="list-group-item list-group-item-action list-group-item-light departments-list d-none"
+                            role="tab" >
+                            <i className="fas fa-file-invoice-dollar mr-2 ml-4"></i>
+                            Finance
+                        </Link>
+
+                        <Link to="/departments/human-resources"
+                            className="list-group-item list-group-item-action list-group-item-light departments-list d-none"
+                            role="tab" >
+                            <i className="fas fa-portrait mr-2 ml-4"></i>
+                            Human Resources
+                        </Link>
+
+                        <Link to="/departments/it-support"
+                            className="list-group-item list-group-item-action list-group-item-light departments-list d-none"
+                            role="tab" >
+                            <i className="fas fa-question-circle mr-2 ml-4"></i>
+                            IT Support
+                        </Link>
+
+                        <Link to="/departments/quality-assurance"
+                            className="list-group-item list-group-item-action list-group-item-light departments-list d-none"
+                            role="tab" >
+                            <i className="fas fa-list-alt mr-2 ml-4"></i>
+                            Quality Assurance
+                        </Link>
+
+                        <NavLink to="/payroll"
+                            className="list-group-item list-group-item-action list-group-item-light" 
+                            role="tab" >
+                            <i className="fas fa-money-bill mr-2"></i>
+                            Payroll
+                        </NavLink>
+
+                        <NavLink to="/leave-management"
+                            className="list-group-item list-group-item-action list-group-item-light"
+                            role="tab" >
+                            <i className="fas fa-bed mr-2"></i>
+                            Leave Management
+                        </NavLink>
+
+                        <NavLink to="/performance"
+                            className="list-group-item list-group-item-action list-group-item-light"
+                            role="tab" >
+                            <i className="fas fa-chart-line mr-2"></i>
+                            Performance
+                        </NavLink>
+
+                        <Link to="#"
                             id="list-recruitment-list" 
                             data-toggle="list" 
                             role="tab" 
-                            aria-controls="recruitment" 
                             className="list-group-item list-group-item-action list-group-item-light"
                             onClick={this.toggleVisibilityRecruitment} >
                                 <i className="fas fa-user-graduate mr-2"></i>
                                 Recruitment
                                 <i className="fas fa-chevron-down float-right pt-1"></i>
-                        </a>
-                        <a 
-                            href="#list-candidate-form" 
-                            id="list-candidate-form-list" 
-                            data-toggle="list" 
+                        </Link>
+
+                        <Link to="/candidate/new"
                             role="tab" 
                             aria-controls="candidate-form" 
                             className="list-group-item list-group-item-action list-group-item-light recruitment-list d-none">
                                 <i className="fas fa-plus-circle mr-2 ml-4"></i>
                                 Add New Candidate
-                        </a>
-                        <a 
-                            href="#list-candidate-edit" 
-                            id="list-candidate-edit-list" 
-                            data-toggle="list" 
+                        </Link>
+
+                        <Link to="/candidate/edit"
                             role="tab" 
                             aria-controls="candidate-edit" 
                             className="list-group-item list-group-item-action list-group-item-light recruitment-list d-none">
                                 <i className="fas fa-user-edit mr-2 ml-4"></i>
                                 Edit Candidate
-                        </a>
-                        <a 
+                        </Link>
+
+                        {/* <a 
                             href="#list-employee-form" 
                             id="list-employee-form-list" 
                             data-toggle="list" 
@@ -186,27 +152,22 @@ class Sidebar extends Component {
                             className="list-group-item list-group-item-action list-group-item-light recruitment-list d-none">
                                 <i className="fas fa-suitcase mr-2 ml-4"></i>
                                 Add New Employee
-                        </a>
-                        <a 
-                            href="#list-equipment" 
-                            id="list-equipment-list" 
-                            data-toggle="list" 
-                            role="tab" 
-                            aria-controls="equipment" 
-                            className="list-group-item list-group-item-action list-group-item-light">
-                                <i className="fas fa-desktop mr-2"></i>
-                                Equipment
-                        </a>
-                        <a 
-                            href="#list-separation" 
-                            id="list-separation-list" 
-                            data-toggle="list" 
-                            role="tab" 
-                            aria-controls="separation" 
-                            className="list-group-item list-group-item-action list-group-item-light">
-                                <i className="fas fa-briefcase mr-2"></i>
-                                Separation
-                        </a>
+                        </a> */}
+
+                        <NavLink to="/equipment"
+                            className="list-group-item list-group-item-action list-group-item-light"
+                            role="tab" >
+                            <i className="fas fa-desktop mr-2"></i>
+                            Equipment
+                        </NavLink>
+
+                        <NavLink to="/separation"
+                            className="list-group-item list-group-item-action list-group-item-light"
+                            role="tab" >
+                            <i className="fas fa-briefcase mr-2"></i>
+                            Separation
+                        </NavLink>
+
                     </div>               
                 </div>
             </>
