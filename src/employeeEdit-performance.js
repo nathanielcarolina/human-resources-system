19db5e12c124d6649ce87b1dff28cd9f14d0e31c
lucedeswede <nathanielcarolina@gmail.com>
2019-12-16 09:35:44 +0100

    Fetch performance data as table

--- src/employeeEdit-performance.js
+++ src/employeeEdit-performance.js
@@ -3,63 +3,63 @@ import React, { Component } from 'react';
 const EmployeeEditPerformance = (props) => {
     if (props.currentEmployee) {
         let currentEmployee = props.currentEmployee;
+        console.log(props.performance);
+        let renderPerformances = props.performance.map(({ Performance_ID, Rating_Date, Rating, Comments, Manager_ID }) =>
+            <tr key={Performance_ID}>
+                <td>{Rating_Date}</td>
+                <td>{Rating}</td>
+                <td>{Comments}</td>
+                {/* <td>{props.getManager(Manager_ID)}</td> */}
+            </tr>
+        ); 
         return(
             <>
                 <h4 class="mb-3">Performance</h4>
-                <form class="needs-validation mb-5" novalidate="">
-                    <div class="row">
-                        <div class="col-md-4 mb-3">
-                            <label htmlFor="empID">Employee ID</label>
-                            <input class="form-control" id="empID" type="text" placeholder={currentEmployee.EmployeeID} readOnly />
-                        </div>
-                        <div class="col-md-4 mb-3">
-                            <label htmlFor="lastName">Last Name</label>
-                            <input type="text" class="form-control" id="lastName" placeholder={currentEmployee.LastName} value="" required="" readOnly />
-                            <div class="invalid-feedback">
-                            Valid last name is required.
-                            </div>
-                        </div>
-                        <div class="col-md-4 mb-3">
-                            <label htmlFor="firstName">First Name</label>
-                            <input type="text" class="form-control" id="firstName" placeholder={currentEmployee.FirstName} value="" required="" readOnly />
-                            <div class="invalid-feedback">
-                            Valid first name is required.
-                            </div>
+                <div class="row mb-4">
+                    <div class="col-md-4 mb-3">
+                        <label htmlFor="empID">Employee ID</label>
+                        <input class="form-control" id="empID" type="text" placeholder={currentEmployee.EmployeeID} readOnly />
+                    </div>
+                    <div class="col-md-4 mb-3">
+                        <label htmlFor="lastName">Last Name</label>
+                        <input type="text" class="form-control" id="lastName" placeholder={currentEmployee.LastName} value="" required="" readOnly />
+                        <div class="invalid-feedback">
+                        Valid last name is required.
                         </div>
                     </div>
-
-                    <hr className="my-4" />
-
-                    <div class="row">
-                        <div class="col-md-6 mb-3">
-                            <label htmlFor="ratingDate">Rating Date</label>
-                            <input class="form-control" id="ratingDate" type="text" placeholder={currentEmployee.Status} readOnly />
-                        </div>
-                        <div class="col-md-6 mb-4">
-                            <label htmlFor="rater">Rater</label>
-                            <input class="form-control" id="rater" type="text" placeholder={currentEmployee.ReportingManager} readOnly />
+                    <div class="col-md-4 mb-3">
+                        <label htmlFor="firstName">First Name</label>
+                        <input type="text" class="form-control" id="firstName" placeholder={currentEmployee.FirstName} value="" required="" readOnly />
+                        <div class="invalid-feedback">
+                        Valid first name is required.
                         </div>
                     </div>
-
-                    <div class="row">
-                        <div class="col-md-6 mb-3">
-                            <label htmlFor="rating">Rating</label>
-                            <input class="form-control" id="rating" type="text" placeholder={currentEmployee.Status} readOnly />
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-12 mb-3">
-                            <label htmlFor="remarks">Remarks</label>
-                            <textarea class="form-control" id="remarks" type="text" rows="3" placeholder={currentEmployee.Status} readOnly></textarea>
-                        </div>
-                    </div>
-                </form>
+                </div>
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Rating Date</th>
+                            <th scope="col">Rating</th>
+                            <th scope="col">Comments</th>
+                            <th scope="col">Rated by</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {renderPerformances}
+                    </tbody>
+                </table>
             </>
         )
       } else {
         return(
-            <div>Loading...</div>
+            <>
+                <div className="text-center">
+                    <div className="mb-3">Loading...</div>
+                    <div class="spinner-border text-secondary" role="status">
+                        <span class="sr-only">Loading...</span>
+                    </div>
+                </div>
+            </>
         )
       
     }
